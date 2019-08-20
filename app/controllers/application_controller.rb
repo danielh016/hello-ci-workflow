0a9ea503b11381782989de006426f7f877a280b7
RainRush <dan3388a@gmail.com>
2019-08-20 21:48:00 +1000

    test http request circle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	# def call(env)
+ #    if env['PATH_INFO'] == '/'
+ #      [301, {'Location' => '/hello'}, []]
+ #    elsif env['PATH_INFO'] == '/hello'
+ #      [200, {'Content-Type' => 'text/plain'}, ['Hello World']]
+ #    else
+ #      [404, {'Content-Type' => 'text/plain'}, ['Not Found']]
+ #    end
+ #  end
 end
